7e8c97a43c004963e8c755c28a3d287d25df410c
Vasilii Generalov <vasilii.generalov@yandex.ru>
2025-07-13 22:54:41 +0300

    Plot regression results
    Signed-off-by: Vasilii Generalov <vasilii.generalov@yandex.ru>

--- linear_regression_1.py
+++ linear_regression_1.py
@@ -3,10 +3,17 @@
 import torch
 import sklearn
 import numpy as np
+import pandas as pd
 
 
 def f(x):
     return 2 * x + 5
+
+
+X_MIN = -10
+X_MAX = 10
+EPOCHS = 100
+SAMPLES = 100
 
 
 class LR(torch.nn.Module):
@@ -16,12 +23,6 @@ class LR(torch.nn.Module):
 
     def forward(self, x):
         return self.fc1(x)
-
-
-X_MIN = -10
-X_MAX = 10
-EPOCHS = 100
-SAMPLES = 100
 
 
 if __name__ == "__main__":
@@ -35,7 +36,9 @@ if __name__ == "__main__":
     opt = torch.optim.SGD(mod.parameters(), lr=0.01)
     obj = torch.nn.MSELoss()
 
-    X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.3)
+    X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(
+        X, Y, test_size=0.3
+    )
 
     train_hst = []
     test_hst = []
@@ -50,3 +53,17 @@ if __name__ == "__main__":
         pred = mod.forward(X_test)
         loss = obj(pred, Y_test)
         test_hst.append(float(loss))
+
+    df = pd.DataFrame(train_hst)
+    df.to_csv("train_loss.csv")
+
+    df = pd.DataFrame(test_hst)
+    df.to_csv("test_loss.csv")
+
+    data = np.ndarray(shape=(SAMPLES, 4))
+    data[:, 0] = torch.squeeze(X, 1).detach().numpy()
+    data[:, 1] = torch.squeeze(f(X), 1).detach().numpy()
+    data[:, 2] = torch.squeeze(Y, 1).detach().numpy()
+    data[:, 3] = torch.squeeze(mod.forward(X), 1).detach().numpy()
+    df = pd.DataFrame(data, columns=["X", "Y", "Ynoise", "Ypred"])
+    df.to_csv("output.csv", index=False)
