b1c4cfe5a465ceacbbff8bdae0f04bbcff2e7d89
Vasilii Generalov <vasilii.generalov@yandex.ru>
2025-07-17 00:07:53 +0300

    Regress sin with activation
    Signed-off-by: Vasilii Generalov <vasilii.generalov@yandex.ru>

---
+++ linear_regression_3/linear_regression_3.py
@@ -0,0 +1,74 @@
+#!/bin/python3
+
+import torch
+import sklearn
+import numpy as np
+import pandas as pd
+
+
+def f(x):
+    return np.sin(x * np.pi / 180)
+
+
+X_MIN = -180
+X_MAX = 180
+EPOCHS = 800
+SAMPLES = 100
+
+
+class NLR(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(1, 1, True)
+        self.ac1 = torch.nn.Sigmoid()
+        self.fc2 = torch.nn.Linear(1, 1, True)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.ac1(x)
+        x = self.fc2(x)
+        return x
+
+
+if __name__ == "__main__":
+    x = np.linspace(X_MIN, X_MAX, SAMPLES)
+    y = f(x) + np.random.normal(0, 0.125, len(x))
+
+    X = torch.FloatTensor(x).reshape(-1, 1)
+    Y = torch.FloatTensor(y).reshape(-1, 1)
+
+    mod = NLR()
+    opt = torch.optim.SGD(mod.parameters(), lr=0.01)
+    obj = torch.nn.MSELoss()
+
+    X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(
+        X, Y, test_size=0.3
+    )
+
+    train_hst = []
+    test_hst = []
+    for _ in range(EPOCHS):
+        mod.zero_grad()
+        pred = mod.forward(X_train)
+        loss = obj(pred, Y_train)
+        train_hst.append(float(loss))
+        loss.backward()
+        opt.step()
+
+        pred = mod.forward(X_test)
+        loss = obj(pred, Y_test)
+        test_hst.append(float(loss))
+
+    df = pd.DataFrame(train_hst)
+    df.to_csv("train_loss.csv")
+
+    df = pd.DataFrame(test_hst)
+    df.to_csv("test_loss.csv")
+
+    data = np.ndarray(shape=(SAMPLES, 4))
+    data[:, 0] = torch.squeeze(X, 1).detach().numpy()
+    data[:, 1] = torch.squeeze(f(X), 1).detach().numpy()
+    data[:, 2] = torch.squeeze(Y, 1).detach().numpy()
+    data[:, 3] = torch.squeeze(mod.forward(X), 1).detach().numpy()
+    df = pd.DataFrame(data, columns=["X", "Y", "Ynoise", "Ypred"])
+    df.to_csv("output.csv", index=False)
