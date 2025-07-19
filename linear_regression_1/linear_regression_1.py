f3397e89d808c26ba78b786141e0081acfed2d1c
Vasilii Generalov <vasilii.generalov@yandex.ru>
2025-07-19 15:43:44 +0300

    Cover model learning
    Signed-off-by: Vasilii Generalov <vasilii.generalov@yandex.ru>

--- linear_regression_1/linear_regression_1.py
+++ linear_regression_1/linear_regression_1.py
@@ -32,23 +32,23 @@ if __name__ == "__main__":
     X = torch.FloatTensor(x).reshape(-1, 1)
     Y = torch.FloatTensor(y).reshape(-1, 1)
 
-    mod = LR()
-    opt = torch.optim.SGD(mod.parameters(), lr=0.01)
-    obj = torch.nn.MSELoss()
-
     X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(
         X, Y, test_size=0.3
     )
 
     train_hst = []
     test_hst = []
+
+    mod = LR()
+    obj = torch.nn.MSELoss()
+    opt = torch.optim.SGD(mod.parameters(), lr=0.01)
     for _ in range(EPOCHS):
         mod.zero_grad()
         pred = mod.forward(X_train)
         loss = obj(pred, Y_train)
-        train_hst.append(float(loss))
         loss.backward()
         opt.step()
+        train_hst.append(float(loss))
 
         pred = mod.forward(X_test)
         loss = obj(pred, Y_test)
