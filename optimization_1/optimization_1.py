5eddb71555e84c8cce910be1ea2de548a784cbbe
Vasilii Generalov <vasilii.generalov@yandex.ru>
2025-07-24 01:10:05 +0300

    Plot 3d data from csv
    Signed-off-by: Vasilii Generalov <vasilii.generalov@yandex.ru>

--- optimization_1/optimization_1.py
+++ optimization_1/optimization_1.py
@@ -10,17 +10,31 @@ X_MIN = -10
 X_MAX = 10
 Y_MIN = -10
 Y_MAX = 10
-SAMPLES = 100
+SAMPLES = 50
+
+
+def f2d(x):
+    return x**2
+
+
+def f3d(x, y):
+    return x**2 + y**2
 
 
 if __name__ == "__main__":
     x = np.linspace(X_MIN, X_MAX, SAMPLES)
-    y = np.linspace(Y_MIN, Y_MAX, SAMPLES)
+    data = np.ndarray(shape=(SAMPLES, 2))
+    data[:, 0] = x
+    data[:, 1] = f2d(x)
+    df = pd.DataFrame(data)
+    df.to_csv("output2d.csv", index=False, header=False)
 
-    data = np.ndarray(shape=(SAMPLES, 4))
+    x = np.repeat(x, SAMPLES)
+    y = np.linspace(Y_MIN, Y_MAX, SAMPLES)
+    y = np.tile(y, SAMPLES)
+    data = np.ndarray(shape=(SAMPLES * SAMPLES, 3))
     data[:, 0] = x
     data[:, 1] = y
-    data[:, 2] = np.vectorize(lambda x: x**2)(x)
-    data[:, 3] = np.vectorize(lambda x, y: (x**2 + y**2) ** (1 / 2))(x, y)
-    df = pd.DataFrame(data, columns=["X", "Y", "P2d", "P3d"])
-    df.to_csv("output.csv", index=False)
+    data[:, 2] = f3d(x, y)
+    df = pd.DataFrame(data)
+    df.to_csv("output3d.csv", index=False, header=False)
